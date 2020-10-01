1febad05b635937a0ecd1e97bf9bdf0c0c61cc4b
colman <cob@example.com>
2020-10-01 23:13:39 +0100

    Began restructing project using blueprints

--- bikebuddy.py
+++ bikebuddy.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import yaml
 from operator_api_client.api_client import *
+from operator_api_client.db_updaters import db_updaters
 
 app = Flask(__name__)
+app.register_blueprint(db_updaters)
 
 
 def read_app_settings():
@@ -20,26 +22,6 @@ app_settings = read_app_settings()
 # JC Decaux & An Rothar Nua both require an API key to be provided
 API_KEY_JC_DECAUX = app_settings['operator_api_keys'].get('jc_decaux')
 API_KEY_AN_ROTHAR_NUA = app_settings['operator_api_keys'].get('an_rothar_nua')
-
-
-@app.route('/getupdates/stations/jcdecaux/<contract>')
-def update_stations_jc_decaux(contract):
-    return update_jc_decaux(contract, API_KEY_JC_DECAUX)
-
-
-@app.route('/getupdates/stations/anrotharnua/<scheme>')
-def update_stations_an_rothar_nua(scheme):
-    return update_an_rothar_nua(scheme, API_KEY_AN_ROTHAR_NUA)
-
-
-@app.route('/getupdates/stations/nextbike/<city>')
-def update_stations_nextbike(city):
-    return update_nextbike(city)
-
-
-@app.route('/getupdates/bikes/bleeperbikes')
-def update_bikes_bleeperbikes():
-    return update_bleeperbikes()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ operator_api_client/db_updaters.py
@@ -0,0 +1,38 @@
+import yaml
+from flask import Blueprint
+from operator_api_client.api_client import *
+
+db_updaters = Blueprint('db_updaters', __name__)
+
+def read_app_settings():
+    """
+    Open and read the YAML config file for the application.
+    :return: The contents of the YAML config file
+    """
+    document = open('config/config.yaml', 'r')
+    return yaml.load(document, Loader=yaml.FullLoader)
+
+
+# Get important configuration details from a .yaml file
+app_settings = read_app_settings()
+
+# JC Decaux & An Rothar Nua both require an API key to be provided
+API_KEY_JC_DECAUX = app_settings['operator_api_keys'].get('jc_decaux')
+API_KEY_AN_ROTHAR_NUA = app_settings['operator_api_keys'].get('an_rothar_nua')
+
+
+@db_updaters.route('/getupdates/stations/jcdecaux/<contract>')
+def update_stations_jc_decaux(contract):
+    return update_jc_decaux(contract, API_KEY_JC_DECAUX)
+
+@db_updaters.route('/getupdates/stations/anrotharnua/<scheme>')
+def update_stations_an_rothar_nua(scheme):
+    return update_an_rothar_nua(scheme, API_KEY_AN_ROTHAR_NUA)
+
+@db_updaters.route('/getupdates/stations/nextbike/<city>')
+def update_stations_nextbike(city):
+    return update_nextbike(city)
+
+@db_updaters.route('/getupdates/bikes/bleeperbikes')
+def update_bikes_bleeperbikes():
+    return update_bleeperbikes()
